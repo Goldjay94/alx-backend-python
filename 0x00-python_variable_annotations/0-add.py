9ec63ebea6c53ab3960255a661339efd23871b02
Goldjay94 <geneviezeh786@gmail.com>
2023-07-06 19:23:03 +0000

    added Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for task 0"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Type annotated function to add 2 floats a and b and
+    return a float"""
+    return a + b
